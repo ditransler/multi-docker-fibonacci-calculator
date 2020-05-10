f631565dbfca3bc1be524dbe689d68101cf16da1
Dmitriy Beglov <d.beglov@galament.com>
2020-05-10 21:37:55 +0400

    fix(client): update title

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Fibonacci Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">OtherPage</Link>
         </header>
